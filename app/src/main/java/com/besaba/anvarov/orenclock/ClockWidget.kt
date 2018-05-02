f54b681e391c2c18c1fea1ee7c87a1a9412e1b04
RatWar <aa960008>
2018-05-02 14:42:43 +0500

    Тестовая версия

--- app/src/main/java/com/besaba/anvarov/orenclock/ClockWidget.kt
+++ app/src/main/java/com/besaba/anvarov/orenclock/ClockWidget.kt
@@ -1,8 +1,14 @@
+@file:Suppress("JAVA_CLASS_ON_COMPANION")
+
 package com.besaba.anvarov.orenclock
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
 import java.text.DateFormat
 import java.util.*
@@ -12,6 +18,8 @@ import java.util.*
  */
 class ClockWidget : AppWidgetProvider() {
 
+    private val updateWidgets = "updateWidgets"
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // There may be multiple widgets active, so update all of them
         for (appWidgetId in appWidgetIds) {
@@ -20,11 +28,32 @@ class ClockWidget : AppWidgetProvider() {
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        super.onEnabled(context)
+        val intent = Intent(context, ClockWidget.javaClass)
+        intent.action = updateWidgets
+        val pIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        val alarmManager: AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 60000, pIntent)
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        super.onDisabled(context)
+        val intent = Intent(context, ClockWidget.javaClass)
+        intent.action = updateWidgets
+        val pIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        val alarmManager: AlarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarmManager.cancel(pIntent)
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (intent?.action.equals(updateWidgets, true)) {
+            val thisAppWidget = ComponentName(context?.packageName, javaClass.name)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            for (appWidgetId in appWidgetManager.getAppWidgetIds(thisAppWidget)) {
+                updateAppWidget(context!!, appWidgetManager, appWidgetId)
+            }
+        }
     }
 
     companion object {
@@ -35,7 +64,7 @@ class ClockWidget : AppWidgetProvider() {
             // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.clock_widget)
             views.setTextViewText(R.id.tvDate, getCurDate())
-            views.setTextViewText(R.id.tvTime,  getCurTime())
+            views.setTextViewText(R.id.tvTime, getCurTime())
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
